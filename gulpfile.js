f5f2fa7bc0195966a0f0d987ec0016d9127be317
Charlie Hawker <charlie.hawker@gmail.com>
2018-03-22 08:19:07 +0000

    Performance improvements made in class

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,28 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var imageOptim = require('gulp-imageoptim');
+var uglify = require('gulp-uglify');
 
 gulp.task('sass', function() {
-return gulp.src('./sass/**/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./css'));
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('./src/images/**/*')
+    .pipe(imageOptim.optimize())
+    .pipe(gulp.dest('./images'));
+});
+
+gulp.task('scripts', function(){
+  return gulp.src('./src/js/**/*')
+    .pipe(uglify())
+    .pipe(gulp.dest('./js'));
 });
 
 gulp.task('default', function(){
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+  gulp.watch('./src/js/**/*.js', ['js']);
 });
